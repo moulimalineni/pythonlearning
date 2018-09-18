301118f40aa723c26adcf3e8ed65a1c705eacb49
moulimalineni <mouli.malineni@gmail.com>
2018-09-18 18:00:11 +0530

    Modified file

--- basics/hello.py
+++ basics/hello.py
@@ -1,1 +1,11 @@
 print("Hello, World")
+a = 2
+print("value of a is", a)
+a = 2.2
+print("Value of a is", a)
+a = "Somestring"
+print("value of a is ", a)
+
+for i in range(10):
+    print("Value of i is", i)
+    
